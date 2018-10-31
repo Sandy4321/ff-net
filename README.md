2778e0ec44791b40561a61fa2dd37a8f1a75718a
Junior Rojas <jrojasdavalos@gmail.com>
2018-10-31 02:49:29 -0600

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# ff-net
-A feedforward neural network learning in real time. [See the live demo.](http://juniorrojas.github.io/ff-net)
-
-![screenshot](media/screenshot.png)
--- js/svg.js
+++
@@ -1,7 +0,0 @@
-var svg = {};
-
-svg.createElement = function(element) {
-	return document.createElementNS("http://www.w3.org/2000/svg", element);
-}
-
-module.exports = svg;
